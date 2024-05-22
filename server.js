234551169fd2d5673910724a95f362cc5d3cb8dd
ertumo ketsebaot <ertumoketsebaot@gmail.com>
2024-05-22 11:45:08 +0300

    uodate

--- server.js
+++ server.js
@@ -45,18 +45,18 @@ const corsOptions = {
   app.use(cors(corsOptions));
   
 
-// app.use((req, res, next) => {
-//     const allowedOrigins = ['http://localhost:3000', 'https://invoice-manager-ketsi.vercel.app'];
-//     const origin = req.headers.origin;
-//     if (allowedOrigins.includes(origin)) {
-//         res.setHeader('Access-Control-Allow-Origin', origin);
-//     }
-//     res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
-//     res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
-//     res.header('Access-Control-Allow-Credentials', 'true');
-//     next();
-//   });
-
+app.use((req, res, next) => {
+    const allowedOrigins = ['http://localhost:3000', 'https://invoice-manager-ketsi.vercel.app'];
+    const origin = req.headers.origin;
+    if (allowedOrigins.includes(origin)) {
+        res.setHeader('Access-Control-Allow-Origin', origin);
+    }
+    res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
+    res.header('Access-Control-Allow-Credentials', 'true');
+    next();
+  });
+  
 app.use(errorHandler);
 
 app.get("/", (req, res) => {
